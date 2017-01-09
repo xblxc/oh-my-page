3b4e3463f8faceaa8c322a6887b87e9f70dcbc61
XiongBo <xb1@meitu.com>
2017-01-09 13:45:19 +0800

    rules can be delete and add tips

--- options/options.js
+++ options/options.js
@@ -12,7 +12,11 @@ var Rule = {
     computed: {
         error: function(){
             var url = this.url;
-            return !url.trim() || url.indexOf('?') != -1 || url.indexOf('#') != -1;
+            return this.create
+                ? !url.trim() || url.indexOf('?') != -1 || url.indexOf('#') != -1
+                : (
+                    url.trim() ? url.indexOf('?') != -1 || url.indexOf('#') != -1 : false
+                  );
         }
     },
     methods: {
@@ -21,6 +25,7 @@ var Rule = {
             var store = window.localStorage;
             var rulesID = ++store.rulesID;
             var self = this;
+            var msg = '';
             var rules;
 
             rules = JSON.parse( store.getItem('rules') );
@@ -35,25 +40,32 @@ var Rule = {
 
                 this.fnReset();
 
+                msg = '创建成功！';
+
             }else{
 
                 var index = rules.findIndex(function(r){
                     return r.id == self.ruleData.id;
                 });
-                rules.splice(index, 1, {
-                    id: this.ruleData.id,
-                    url: this.url,
-                    jscode: this.jscode
-                })
+
+                if( this.url.trim() ){
+                    rules.splice(index, 1, {
+                        id: this.ruleData.id,
+                        url: this.url,
+                        jscode: this.jscode
+                    });
+                    msg = '保存成功！';
+                }else {
+                    rules.splice(index, 1);
+                    msg = '删除成功！';
+                }
 
                 this.pristine = true;
             }
 
             store.setItem('rules', JSON.stringify(rules));
 
-            App.$emit('update-rules');
-
-            alert('操作成功~');
+            App.$emit('update-rules', {msg: msg});
         },
         fnAltered: function(){
             this.pristine = false;
@@ -67,13 +79,36 @@ var Rule = {
     }
 };
 
+var Noty = {
+    name: 'noty',
+    template: '<transition name="noty"><div class="noty alert-success" v-show="show_status">{{msg}}</div></transition>',
+    data: function(){
+        return {
+            show_status: false,
+            msg: '',
+        }
+    },
+    props: ['timeout'],
+    methods: {
+        show: function(msg){
+            var self = this;
+            this.show_status = true;
+            this.msg = msg;
+            setTimeout(function(){
+                self.show_status = false;
+            }, this.timeout || 0);
+        }
+    }
+};
+
 localStorage.rules = localStorage.rules || '[]';
 localStorage.rulesID = localStorage.rulesID || 0;
 
 var App = new Vue({
     el: '#app',
     components: {
-        rule: Rule
+        rule: Rule,
+        noty: Noty
     },
     data: {
         rules: window.localStorage.rules ? JSON.parse(window.localStorage.rules) : [],
@@ -94,9 +129,10 @@ var App = new Vue({
         fnShowCreateRuleForm: function(){
             this.showCreateRuleForm = true;
         }
-    }
+    },
 }).$on('hide-create-rule-form', function(){
     this.showCreateRuleForm = false;
-}).$on('update-rules', function(){
+}).$on('update-rules', function(data){
     this.rules = JSON.parse(localStorage.rules);
+    this.$refs.noty.show(data.msg);
 })
